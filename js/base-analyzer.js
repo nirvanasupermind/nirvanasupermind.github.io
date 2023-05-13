8079928f891b1938fda37cd0a471493515bd9c67
nirvanasupermind <nirvana.supermind@gmail.com>
2023-05-12 21:28:27 -0700

    another fix

--- js/base-analyzer.js
+++ js/base-analyzer.js
@@ -118,10 +118,10 @@ function getSDNName(base) {
     if(result[result.length - 1] === "d") {
         result += "r";
     }
-    else if(result[result.length - 1] === "i") {
+    else if(result.slice(-4) === "quad") {
         result += "n";
     }   
-    else if(result[result.length - 1] === "n") {
+    else if(result.slice(-3) === "nil") {
         result += "i";
     } 
     
